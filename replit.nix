f4c635375ba1d66291515daf5b4a9a1ee8fd9967
lyfe00011 <lyfe00011@gmail.com>
2024-06-29 13:40:51 +0530

    fix

--- replit.nix
+++ replit.nix
@@ -3,7 +3,6 @@
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.ffmpeg
-        pkgs.replitPackages.jest
         pkgs.libwebp
         pkgs.imagemagick
         pkgs.git
