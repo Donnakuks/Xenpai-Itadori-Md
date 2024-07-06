8bd82be2e8565abb7ec5b05204d142db50d4602b
lyfe00011 <lyfe00011@gmail.com>
2024-07-06 23:25:49 +0530

    fix

--- plugins/bing.js
+++ plugins/bing.js
@@ -13,7 +13,7 @@ bot(
         `Please set a bing cookie, log in to bing.com/chat, use bing AI chat once, and then copy the cookie.`
       )
     match = match || message.reply_message.text
-    if (!match) return await message.send('*Example : bing Hi*')
+    if (!match) return await message.send('*Example :* bing Hi')
     await message.send(
       {
         text: '⏳',
@@ -37,7 +37,7 @@ bot(
 
 bot(
   {
-    pattern: 'dali ?(.*)',
+    pattern: 'dale ?(.*)',
     fromMe: true,
     desc: 'bing image creator',
     type: 'ai',
@@ -47,7 +47,11 @@ bot(
       return await message.send(
         `Please set a bing cookie, log in to https://bing.com/images/create, use bing Image Creator once, and then copy the cookie.`
       )
+    if (!match)
+      return await message.send(
+        '*Example :* dale Create a 3D illusion for a WhatsApp profile picture where a boy in a white shirt sits casually on a royal Sofa. Wearing White sneakers,a black T-shirt, and sunglasses, he looks ahead. The background features “Arjun ” in big and capital Yellow fonts on the black wall.'
+      )
     const res = await dall3(match)
-    return await message.sendFromUrl(res)
+    return await message.sendFromUrl(res.data)
   }
 )
